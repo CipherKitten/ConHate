cc880c488532eb785fb533ad95de8557fa6fe63f
qff <wretchedfish@tutamail.com>
2025-09-25 10:27:46 +0100

    made it use a random meme

--- src/isrc/bot.c
+++ src/isrc/bot.c
@@ -42,8 +42,6 @@ void* send_meme(void* args){
 
     if(fiftyfifty < 2) return NULL;
 
-<<<<<<< HEAD
-=======
     char* links[] = {"https://i.kym-cdn.com/photos/images/newsfeed/002/931/607/1ff"};
     int linksLen = sizeof(links)/sizeof(links[0]);
 
@@ -55,7 +53,6 @@ void* send_meme(void* args){
 
     snprintf(msgContent, maxTextSize, "Check this out[:](%s)", links[linkIdx]);
 
->>>>>>> 06f6e5e (made it use a random meme)
     struct default_message_work* msg = (struct default_message_work*)args;
     const struct discord_message* message = msg->message;
     struct discord* client = msg->client;
@@ -63,17 +60,11 @@ void* send_meme(void* args){
     sleep(5);
     struct discord_create_message params = {0};
     struct discord_ret_message ret = {0};
-<<<<<<< HEAD
-    params.content = "Le J.D. Vance[:](https://i.kym-cdn.com/photos/images/newsfeed/002/931/607/1ff)";
-    
-    discord_create_message(client, message->channel_id, &params, &ret );
-=======
     params.content = msgContent;
     
     discord_create_message(client, message->channel_id, &params, &ret );
     free(msgContent);
 
->>>>>>> 06f6e5e (made it use a random meme)
 
     return NULL;
 }
